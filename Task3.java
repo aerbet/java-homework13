93107c70ddb6285513313a5025d4d9fbb351c9fc
Erbol Zhaparov <ae.270718@gmail.com>
2025-08-07 17:49:26 +0600

    some commits from work

--- Task3.java
+++ Task3.java
@@ -1,6 +1,16 @@
 public class Task3 {
     
     public static void main(String[] args) {
+        int start = 1984;
+        int cicle = 60;
+        int minCicle = 12;
+
+        String[] colors = {"зеленый", "красный", "желтый", "белый", "черный"};
+        String[] animals = {"крыса", "корова", "тигр", "кролик", "дракон", "змея", "лошадь", "овца", "обезьяна", "курица", "собака", "свинья"};
         
+
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i]);
+        }
     }
 }
