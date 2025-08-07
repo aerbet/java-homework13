7ed1e32918885db714f9f0a898626b3ed55d8249
Erbol Zhaparov <ae.270718@gmail.com>
2025-08-07 13:15:40 +0600

    finished task 2

--- Task2.java
+++ Task2.java
@@ -1,34 +1,62 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Task2 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите число от 10 до 1000000: ");
         int num = sc.nextInt();
-        int result = num;
+        int numLength = 0;
+        int numTemp = num;
+        int overall = 0;
+        double amf = 0;
+
+        int evenIndex = 0, oddIndex = 0;
 
         int min = 10;
         int max = 1000000;
-
-        long[] arr = new long[0];
-
-        while (result > 0) {
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = result % 10;
+        
+        if (num > max || num < min) {
+            System.out.println("Вы ввели число вне указанного диапозона, попробуйте ещё раз.");
+        } else {
+            while (numTemp > 0) {
+                numLength++;
+                numTemp /= 10;
             }
 
-            result /= 10;
-        }
+            int[] arr = new int[numLength];
+            int[] evenNums = new int[numLength];
+            int[] oddNums = new int[numLength];
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
+            for (int i = 0; i < numLength; i ++) {
+                arr[i] = num % 10;
+                num /= 10;
 
+                overall += arr[i];
+
+                if (arr[i] % 2 == 0) {
+                    evenNums[evenIndex++] = arr[i];
+                } else {
+                    oddNums[oddIndex++] = arr[i];
+                }
             }
-        }
 
-        System.out.println(Arrays.toString(arr));
+            Arrays.sort(arr);
+
+            amf = ((double) arr[0] + arr[arr.length - 1]) / 2;
+
+
+            String tmpl = "%s: %s\n";
+
+            System.out.printf(tmpl, "Количество цифр в числе: ", numLength);
+            System.out.printf(tmpl, "Сумма всех цифр в числе: ", overall);
+            System.out.printf(tmpl, "Четные цифры: ", Arrays.toString(Arrays.copyOf(evenNums, evenIndex)));
+            System.out.printf(tmpl, "Нечетные цифры: ", Arrays.toString(Arrays.copyOf(oddNums, oddIndex)));
+            System.out.printf(tmpl, "Среднее арифметичсекое минимального и максимального числа: ", amf);
+
+            sc.close();
+        }
     }
 }
